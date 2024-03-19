a6f026b6c843187dc493b1cc4300235bf175b392
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2024-03-19 10:52:41 +0700

    feat: Add arsip and detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Arsip from "./pages/Arsip";
+import DetailPage from "./pages/DetailPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/arsip" element={<Arsip />} />
+        <Route path="/note/:noteId" element={<DetailPage />} />
       </Routes>
     </Router>
   );
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,11 @@
+import Navbar from "./Navbar";
+
 /* eslint-disable react/prop-types */
 const Container = ({ children }) => {
   return (
     <>
       {" "}
-      <div className="flex flex-row justify-between items-center bg-black text-white py-5 px-4 shadow-sm">
-        <h1 className="font-semibold text-lg">Aplikasi catatan</h1>
-        <h1 className="font-semibold text-lg">Arsip</h1>
-      </div>
+      <Navbar />
       <div className="px-4 py-6 flex flex-col justify-center items-center w-full">
         <div className=" w-full lg:max-w-5xl md:max-w-3xl">{children}</div>
       </div>
