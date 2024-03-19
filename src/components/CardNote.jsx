1092901d2ed93ae5170d1e2476f324f772688605
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2024-03-19 09:49:16 +0700

    feat: Add card note

---
+++ src/components/CardNote.jsx
@@ -0,0 +1,22 @@
+const CardNote = () => {
+  return (
+    <>
+      <div className="bg-gray-200 border border-gray-200 p-4 rounded-md shadow-md min-h-[180px] lg:min-h-[230px] flex flex-col justify-between space-y-3 cursor-pointer hover:opacity-80 transition duration-200 ease-in-out">
+        <div className="flex flex-col space-y-4">
+          <h1 className="font-semibold text-lg line-clamp-1">
+            juddsdadsd sasa{" "}
+          </h1>
+          <p className="line-clamp-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            repellendus! Lorem ipsum
+          </p>
+        </div>
+        <div>
+          <p>21/02/2024</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardNote;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
+import CardNote from "../components/CardNote";
 import Container from "../components/Container";
 
 const Home = () => {
   return (
     <Container>
-      <h1>Hello</h1>
+      <div className="flex flex-col  ">
+        <input
+          type="text"
+          placeholder="Cari berdasarkan judul"
+          className="p-3 rounded-md border-2 border-gray-200 outline-none focus:border-gray-500 w-full"
+        />
+        <h1 className="text-center font-semibold mt-6 text-xl mb-6">Notes</h1>
+        <div className="flex flex-col space-y-5 md:grid md:grid-cols-3 md:gap-5 md:space-y-0 lg:grid-cols-4">
+          <CardNote />
+          <CardNote />
+          <CardNote />
+          <CardNote />
+        </div>
+      </div>
     </Container>
   );
 };
